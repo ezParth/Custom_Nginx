65ad30210d35f8aee31e91affabbaecd26c479fc
ezParth <parthsingh55m@gmail.com>
2025-08-18 12:38:58 +0530

    added worker nodes to nginx

--- dist/server.js
+++ dist/server.js
@@ -8,19 +8,37 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const config_1 = require("./config");
 const commander_1 = require("commander");
-// const mainPath = path.join(__dirname, '../config.yml')
-// console.log(mainPath)
+const node_cluster_1 = __importDefault(require("node:cluster"));
+const os_1 = __importDefault(require("os"));
+const createServer = (config) => __awaiter(void 0, void 0, void 0, function* () {
+    const { workerCount } = config;
+    const workers = new Array(workerCount);
+    if (node_cluster_1.default.isPrimary) {
+        console.log("Master Process in up!");
+        for (var i = 0; i < workerCount; i++) {
+            node_cluster_1.default.fork();
+            console.log(`Woker node ${i + 1} spinned up`);
+        }
+    }
+    else {
+        console.log(`Worker node`);
+    }
+});
 const main = () => __awaiter(void 0, void 0, void 0, function* () {
+    var _a;
     try {
         commander_1.program.option('--config <path>');
         commander_1.program.parse();
         const options = commander_1.program.opts();
         if (options && 'config' in options) {
             const validatedConfig = yield (0, config_1.validateConfig)(yield (0, config_1.parseYAMLConfig)(options.config));
-            console.log("-------->\n", validatedConfig);
+            yield createServer({ port: validatedConfig.server.listen, workerCount: (_a = validatedConfig.server.workers) !== null && _a !== void 0 ? _a : os_1.default.cpus().length });
         }
     }
     catch (error) {
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,28 @@
 import {parseYAMLConfig, validateConfig} from "./config";
 import { program } from "commander";
-import path from "path"
-// const mainPath = path.join(__dirname, '../config.yml')
-// console.log(mainPath)
+import cluster from "node:cluster";
+import os from "os"
+
+interface createServerConfig {
+    port: number
+    workerCount: number
+}
+
+const createServer = async (config: createServerConfig) => {
+    const { workerCount } = config
+    const workers = new Array(workerCount)
+
+    if(cluster.isPrimary) {
+        console.log("Master Process in up!")
+
+        for(var i = 0; i<workerCount; i++) {
+            cluster.fork()
+            console.log(`Woker node ${i+1} spinned up`)
+        }
+    } else {
+        console.log(`Worker node`)
+    }
+}
 
 const main = async () => {
     try {
@@ -11,7 +31,8 @@ const main = async () => {
         const options = program.opts()
         if(options && 'config' in options) {
             const validatedConfig = await validateConfig(await parseYAMLConfig(options.config))
-            console.log("-------->\n",validatedConfig)
+
+            await createServer({ port: validatedConfig.server.listen, workerCount: validatedConfig.server.workers ?? os.cpus().length})
         }
     } catch (error) {
         console.log("Error in server.ts file: ", error)        
